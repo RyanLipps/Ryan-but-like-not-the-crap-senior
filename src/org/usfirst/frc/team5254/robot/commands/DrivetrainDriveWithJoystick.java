3a952516b7c1d12ae08e057372b6f33a4bea0e0b
Ryan Lippert <ryan.home.popsicle@gmail.com>
2018-01-31 18:54:29 -0500

    Wowza

---
+++ src/org/usfirst/frc/team5254/robot/commands/DrivetrainDriveWithJoystick.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team5254.robot.commands;
+
+import java.awt.Robot;
+
+import org.usfirst.frc.team5254.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class DrivetrainDriveWithJoystick extends Command {
+
+    public DrivetrainDriveWithJoystick() {
+       requires(Robot.Drivetrain);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.Drivetrain.drive(Robot.oi.driverJoystick.getRawAxis(RobotMap.DRIVER_THROTTLE_AXIS),
+    			Robot.oi.driverJoystick.getRawAxis(RobotMap.DRIVER_TURN_AXIS));
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
