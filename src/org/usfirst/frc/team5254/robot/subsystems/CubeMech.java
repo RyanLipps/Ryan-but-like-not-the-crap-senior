05aefb78b58b977456a80f3f43da9d03756e1039
Ryan Lippert <ryan.home.popsicle@gmail.com>
2018-02-03 12:17:46 -0500

    First Subsystem

--- src/org/usfirst/frc/team5254/robot/subsystems/CubeMech.java
+++ src/org/usfirst/frc/team5254/robot/subsystems/CubeMech.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team5254.robot.subsystems;
 
+import org.usfirst.frc.team5254.robot.commands.CubeMechStopFlywheels;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,12 +10,35 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class CubeMech extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
-
+	public static VictorSP cubeMechLeftFlywheels = new VictorSP(Robotmap.CUBE_MECH_Left);
+	public static VictorSP cubeMechRightFlywheels = new VictorSP(RobotMap.Cube_MECH_RIGHT);
+	
+	public static Solenoid cubeMechArms = new Solenoid(RobotMap.Cube_Mech_Arms);
+	public static DoubleSolenoid CubeMechHinge = new DoubleSolenoid(Robot.Map.Cube_Mech_Hinge_Up, Robot.Map.Cube.Mech.Hinge.Down);
+	
+	public CubeMech () {
+	}
+	
+	public void ArmsDown() {
+		cubeMechHinge.set(DoubleSolenoid.Value.kReverse);
+	}
+	public void ArmsUp() {
+		cubeMechHinge.set(DoubleSolenoid.Value.kForward);
+	}
+	public void Intake() {
+		cubeMechLeftFlywheels.set(1);
+		cubeMechRightFlywheels.set(1);
+	}
+	public void Outake() {
+		cubeMechLeftFlywheels.set(-1);
+		cubeMechRightFlywheels.set(-1);
+	}
+	public void StopFlywheels() {
+		cubeMechLeftFlywheels.set(0);
+		cubeMechRightFlywheels.set(0);
+	}
     public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
+    	setDefaultCommand(new CubeMechStopFlywheels());
     }
 }
 
