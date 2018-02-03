33d60dcd757d72cae14d7d0e98c74566e8a5809d
Ryan Lippert <ryan.home.popsicle@gmail.com>
2018-02-03 14:16:02 -0500

    stuff

--- src/org/usfirst/frc/team5254/robot/RobotMap.java
+++ src/org/usfirst/frc/team5254/robot/RobotMap.java
@@ -2,20 +2,30 @@
 
 package org.usfirst.frc.team5254.robot;
 
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
 public class RobotMap {
 	
 	//Motor Controllers
+	public static final int DRIVETRAIN_LEFT = 0;
+	public static final int DRIVETRAIN_LEFT2 = 1;
+	public static final int DRIVETRAIN_RIGHT = 2;
+	public static final int DRIVETRAIN_RIGHT2 = 3;
+	public static final int CUBE_MECH_LEFT = 4;
+	public static final int CUBE_MECH_RIGHT = 5;
+	public static final int ELEVATOR = 6;
 		
 	//Joysticks
-		
-	//axis
-		
-	//pistons
+	public static final int DRIVER_THROTTLE_AXIS = 1;
+	public static final int DRIVER_TURN_AXIS = 4;
+	public static final int OPERATOR_THROTTLE_AXIS = 5;
 	
+	//axis
+	public static final int SHIFTING_PISTON = 0;
+	public static final int CUBE_MECH_ARMS = 1;
+	public static final int CUBE_MECH_HINGE_UP = 2;
+	public static final int CUBE_MECH_HINGE_DOWN = 3;
+	
+	//pistons
+	public static final double DRIVETRAIN_GEAR_RATIO = ((25.0 / 6.0) * (100.0 / 91.0));
+	public static final int DRIVETRAIN_WHEEL_DIAMETER = 6
+			
 }
