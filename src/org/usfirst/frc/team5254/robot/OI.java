8c50b2e0d58b60981186744eb1f028fb31006a08
Ryan Lippert <ryan.home.popsicle@gmail.com>
2018-02-03 14:15:34 -0500

    stuff

--- src/org/usfirst/frc/team5254/robot/OI.java
+++ src/org/usfirst/frc/team5254/robot/OI.java
@@ -1,9 +1,39 @@
 
 package org.usfirst.frc.team5254.robot;
 
+import org.usfirst.frc.team5254.robot.commands.CubeMechIntake;
+import org.usfirst.frc.team5254.robot.commands.CubeMechOutake;
+import org.usfirst.frc.team5254.robot.commands.CubeMechStopFlywheels;
+import org.usfirst.frc.team5254.robot.commands.DrivetrainDriveWithJoystick;
+import org.usfirst.frc.team5254.robot.commands.DrivetrainShiftDown;
+import org.usfirst.frc.team5254.robot.commands.DrivetrainShiftUp;
+import org.usfirst.frc.team5254.robot.commands.DrivetrainSlowTurn;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+
 public class OI {
 	
 	public OI() {
+		public Joystick driverJoystick = new Joystick(RobotMap.DRIVER_JOYSTICK);
+		public Joystick operatorJoystick = new Joystick(RobotMap.OPERATOR_JOYSTICK);
+		
+		public OI() {
+			//defining buttons
+			Button OperatorButtonA = new JoystickButton(operatorJoystick, 1);
+			Button OperatorButtonX = new JoystickButton(operatorJoystick, 3);
+			Button OperatorButtonB = new JoystickButton(operatorJoystick, 2);
+			Button DriverButtonRB = new JoystickButton(driverJoystick, 6);
+			Button DriverButtonRJC = new JoystickButton(driverJoystick, 10);
+			//Sub Commands  
+			DriverButtonRB.whenPressed(new DrivetrainShiftUp());
+			DriverButtonRB.whenInactive(new DrivetrainShiftDown());
+			DriverButtonRJC.whenPressed(new DrivetrainSlowTurn());
+			DriverButtonRJC.whenInactive(new DrivetrainDriveWithJoystick());
+			OperatorButtonA.whenPressed(new CubeMechIntake());
+			OperatorButtonX.whenPressed(new CubeMechOutake());
+			OperatorButtonB.whenPressed(new CubeMechStopFlywheels());
 		
 	}
 	
--- src/org/usfirst/frc/team5254/robot/Robot.java
+++ src/org/usfirst/frc/team5254/robot/Robot.java
@@ -5,9 +5,23 @@ package org.usfirst.frc.team5254.robot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+
+import org.usfirst.frc.team5254.robot.subsystems.CubeMech;
+import org.usfirst.frc.team5254.robot.subsystems.Drivetrain;
+import org.usfirst.frc.team5254.robot.subsystems.Elevator;
+
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
+
+	static Timer timer = new Timer();
+	
+	Command autonomousCommands;
+	
+	public static OI oi;
+	public static Drivetrain Drivetrain = new Drivetrain();
+	public static CubeMech CubeMech = new CubeMech();
+	public static Elevator Elevator = new Elevator();
 	
 	
 	@Override
